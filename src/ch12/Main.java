533237249607f1f42bcbe27d7c6cf3fabf2744ee
Aejin Kang <dowlsdl003@naver.com>
2023-06-28 16:59:41 +0900

    주석 추가

--- src/ch12/Main.java
+++ src/ch12/Main.java
@@ -6,6 +6,6 @@ public class Main {
         MyService srv2 = new MyService("학생 정보 관리 서비스");
 
         srv1.start(); // 스레드에 start구현되어 있음 상속 받아서 MyService가 사용 가능 // 스레드 시작
-        srv2.start();
+        srv2.start(); //다른 스레드 시작
     }
 }
